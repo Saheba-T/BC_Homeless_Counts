0bad597bde1b8d8f6b5dcf3d1d47648a9be4a7f3
Saheba-T <saheba.tegally@hotmail.com>
2023-03-01 01:24:38 -0800

    "Removed the comparison to 2018 component and rearranged the layout to make it user-friendly"

--- code/03_shiny_app/ui.R
+++ code/03_shiny_app/ui.R
@@ -2,29 +2,24 @@
 
 
 ui <- dashboardPage(
-  dashboardHeader(title = "B.C. Homeless Counts"),
+  dashboardHeader(title = "B.C. Homeless Counts", titleWidth = 245),
   
-  dashboardSidebar(
+  dashboardSidebar(width = 245,
     sidebarMenu(
-      
-      menuItem("Basic Overview", 
-               tabName = "overview"
-               ),
-      menuItem("More Details",
-               menuSubItem("Sub1", tabName = "sub1"),
-               menuSubItem("Sub2",tabName = "sub2" )
-               ),
-      menuItem("About",
-               tabName = "about"),
-      menuItem(h5("Comparison to 2018:"),
-               checkboxInput(inputId = "comparison_to_2018",
-                             label = "Compare", 
-                             value = FALSE)
-               ),
       menuItem(selectInput(inputId = "homeless_type",
                            label = "Homeless Type",
                            choices = c("Respondents","Sheltered", "Unsheltered"))
-               )
+      ),
+      menuItem("Basic Demographic", 
+               tabName = "overview"
+               ),
+      menuItem("More Details",
+               menuSubItem("History of Homelessness", tabName = "sub1"),
+               menuSubItem("Health-Related Information",tabName = "sub2" )
+               ),
+      menuItem("About",
+               tabName = "about")
+      
     )
   ),
   
@@ -47,22 +42,23 @@ ui <- dashboardPage(
                        h4("Age"),
                        plotOutput("age_distn")
                        ),
-                column(width = 6,
-                       h4("Racial Identity"),
-                       plotOutput("racial_identity_distn")
-                       ),
                 column(width = 3,
                        h4("Gender"),
                        plotOutput("gender_distn")
+                ),
+                column(width = 6,
+                       h4("Racial Identity"),
+                       plotOutput("racial_identity_distn")
                        )
                 ),
-              
               fluidRow(h4("Source of Income"),
                        plotOutput("source_of_income_distn"))
               
       ),
       
       tabItem(tabName = "sub1",
+              fluidRow(h4("Loss of Housing (More than 1 response possible)"),
+                       plotOutput("housing_loss")),
               fluidRow(box(plotOutput("age_when_first_homeless")),
                        box(plotOutput("homeless_period"))
                        ),
@@ -72,21 +68,24 @@ ui <- dashboardPage(
                               tabPanel("Sheltered",tableOutput("place_of_stay_sheltered")),
                               tabPanel("Unsheltered",tableOutput("place_of_stay_unsheltered"))
                               )
-                       ),
-              fluidRow(
-                box(box(tableOutput("housing_loss_top10_table")))
-              )
+                       )
               
       ),
       
       tabItem(tabName = "sub2",
-              fluidRow(box(plotOutput("health_condition_distn")),
-                       box(plotOutput("num_health_conditions_distn"))
+              fluidRow(
+                column(width = 7,
+                       h4("Health Condition"),
+                       plotOutput("health_condition_distn")
                        ),
-              fluidRow(box(width = 12,
-                           plotOutput("services_accessed"))
+                column(width = 5,
+                       h4("Total Number of Health Conditions"),
+                       plotOutput("num_health_conditions_distn")
+                       )),
+              fluidRow(h4("Services Accessed in Previous 12 Months"),
+                       plotOutput("services_accessed")
                        )
-      ),
+              ),
       
       tabItem(tabName = "about",
               div(includeMarkdown("about.md")))
